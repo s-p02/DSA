02f78f9b4e1deff92fe1b1c2a5e9a73202c27aa7
fudon <80692534+s-p02@users.noreply.github.com>
2022-09-03 21:09:57 +0530

    Time: 7 ms (58.87%), Space: 10.8 MB (32.56%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
@@ -12,22 +12,56 @@ class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
         
-        ListNode* temp= head;
-        long long size=1;
-        while(temp->next!=NULL){
-            size ++;
-            temp=temp->next;
+        ListNode* dummy = new ListNode();
+        dummy->next= head;
+        ListNode* slow=dummy;
+        ListNode* fast= dummy;
+        for(int i=1;i<=n;i++){
+            fast=fast->next;
         }
-        if(size== n){
-            head=head->next;
-            return head;
+        while(fast->next!=NULL){
+            slow= slow->next;
+            fast= fast->next;
         }
-        temp= head;
-        for(int i=1; i<(size-n+1)-1;i++){
-            temp=temp->next;   
-        }
-        temp->next= temp->next->next;
-        return head;
+        slow->next= slow->next->next;
+        ListNode* newHead= dummy->next;
+        return newHead;
         
     }
 };
+
+
+// Naive Approach T.C = 0(2N) since there were two passes
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode() : val(0), next(nullptr) {}
+//  *     ListNode(int x) : val(x), next(nullptr) {}
+//  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     ListNode* removeNthFromEnd(ListNode* head, int n) {
+        
+//         ListNode* temp= head;
+//         long long size=1;
+//         while(temp->next!=NULL){
+//             size ++;
+//             temp=temp->next;
+//         }
+//         if(size== n){
+//             head=head->next;
+//             return head;
+//         }
+//         temp= head;
+//         for(int i=1; i<(size-n+1)-1;i++){
+//             temp=temp->next;   
+//         }
+//         temp->next= temp->next->next;
+//         return head;
+        
+//     }
+// };
