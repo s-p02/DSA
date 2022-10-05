b3e742d99bdd0425c0cbad86f2f8aa570ff197b3
fudon <80692534+s-p02@users.noreply.github.com>
2022-10-05 18:13:53 +0530

    Time: 0 ms (100.00%), Space: 6 MB (38.67%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,19 +1,34 @@
-//tablulation
 class Solution {
 public:
     int fib(int n) {
-        if(n<2){
-            return n;
-        }
-        vector<int>dp(n+1,-1);
-        dp[0]=0;
-        dp[1]=1;
+        if(n<2) return n;
+        int prev2=0;
+        int prev=1;
+        int curr;
         for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+            curr= prev+prev2;
+            prev2=prev;
+            prev=curr;
         }
-        return dp[n];
+        return curr;
     }
 };
+// //tablulation
+// class Solution {
+// public:
+//     int fib(int n) {
+//         if(n<2){
+//             return n;
+//         }
+//         vector<int>dp(n+1,-1);
+//         dp[0]=0;
+//         dp[1]=1;
+//         for(int i=2;i<=n;i++){
+//             dp[i]=dp[i-1]+dp[i-2];
+//         }
+//         return dp[n];
+//     }
+// };
 
 // // memoization
 // class Solution {
