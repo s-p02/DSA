8eb6e8988af59c0777aee2ab437dab30b635abf4
fudon <80692534+s-p02@users.noreply.github.com>
2022-10-15 14:52:31 +0530

    Time: 11 ms (60.42%), Space: 9 MB (51.94%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,17 +1,39 @@
+// Tabulation
 class Solution {
 public:
-    int cntmin(int i,int j,int n,vector<vector<int>>& triangle,vector<vector<int>>& dp){
-        if(i==n-1) return triangle[i][j];
-        if(dp[i][j]!=-1) return dp[i][j];
-        
-        int down= triangle[i][j]+ cntmin(i+1,j,n,triangle,dp);
-        int diag= triangle[i][j]+ cntmin(i+1,j+1,n,triangle,dp);
-        
-        return dp[i][j]= min(down,diag);
-    }
     int minimumTotal(vector<vector<int>>& triangle) {
         int n= triangle.size();
         vector<vector<int>> dp(n,vector<int>(n,-1));
-        return cntmin(0,0,n,triangle,dp);
+        //base case:
+        for(int j=0;j<n;j++){
+            dp[n-1][j]= triangle[n-1][j];
+        }
+        for(int i=n-2;i>=0;i--){
+            for(int j=0;j<=i;j++){
+                int down= triangle[i][j]+dp[i+1][j];
+                int diag= triangle[i][j]+dp[i+1][j+1];
+                dp[i][j]=min(down,diag);
+            }
+        }
+        return dp[0][0];
     }
 };
+
+// Memoization
+// class Solution {
+// public:
+//     int cntmin(int i,int j,int n,vector<vector<int>>& triangle,vector<vector<int>>& dp){
+//         if(i==n-1) return triangle[i][j];
+//         if(dp[i][j]!=-1) return dp[i][j];
+        
+//         int down= triangle[i][j]+ cntmin(i+1,j,n,triangle,dp);
+//         int diag= triangle[i][j]+ cntmin(i+1,j+1,n,triangle,dp);
+        
+//         return dp[i][j]= min(down,diag);
+//     }
+//     int minimumTotal(vector<vector<int>>& triangle) {
+//         int n= triangle.size();
+//         vector<vector<int>> dp(n,vector<int>(n,-1));
+//         return cntmin(0,0,n,triangle,dp);
+//     }
+// };
