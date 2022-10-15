95dc8bd32b5fde347a11bd86c2f045fc4f1119c8
fudon <80692534+s-p02@users.noreply.github.com>
2022-10-15 14:55:50 +0530

    Time: 18 ms (17.37%), Space: 8.9 MB (60.48%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,23 +1,48 @@
-// Tabulation
+// space optimization
 class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n= triangle.size();
-        vector<vector<int>> dp(n,vector<int>(n,-1));
+        vector<int> nex(n,0);
         //base case:
         for(int j=0;j<n;j++){
-            dp[n-1][j]= triangle[n-1][j];
+            nex[j]= triangle[n-1][j];
         }
         for(int i=n-2;i>=0;i--){
+            vector<int> curr(n,0);
             for(int j=0;j<=i;j++){
-                int down= triangle[i][j]+dp[i+1][j];
-                int diag= triangle[i][j]+dp[i+1][j+1];
-                dp[i][j]=min(down,diag);
+                int down= triangle[i][j]+nex[j];
+                int diag= triangle[i][j]+nex[j+1];
+                curr[j]=min(down,diag);
             }
+            nex=curr;
         }
-        return dp[0][0];
+        return nex[0];
     }
 };
+
+// // Tabulation
+// class Solution {
+// public:
+//     int minimumTotal(vector<vector<int>>& triangle) {
+//         int n= triangle.size();
+//         vector<vector<int>> dp(n,vector<int>(n,-1));
+//         //base case:
+//         for(int j=0;j<n;j++){
+//             dp[n-1][j]= triangle[n-1][j];
+//         }
+//         for(int i=n-2;i>=0;i--){
+//             for(int j=0;j<=i;j++){
+//                 int down= triangle[i][j]+dp[i+1][j];
+//                 int diag= triangle[i][j]+dp[i+1][j+1];
+//                 dp[i][j]=min(down,diag);
+//             }
+//         }
+//         return dp[0][0];
+//     }
+// };
+
+
 
 // Memoization
 // class Solution {
