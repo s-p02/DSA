e14093f409c08e0deb55315fa0dfe4a26844423a
fudon <80692534+s-p02@users.noreply.github.com>
2022-10-26 14:42:50 +0530

    Time: 818 ms (15.58%), Space: 12.8 MB (67.31%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,26 +1,30 @@
+// Tabulation
 class Solution {
 public:
-    
-    bool subsetSum(int ind, int target,vector<int>& nums,vector<vector<int>> &dp){
-        if(target==0) return true;
+    bool subsetSum(vector<int>& nums, int target, int n){
+        vector<vector<bool>> dp(n, vector<bool>(target+1,false));
         
-        if(ind==0){
-            if(nums[ind]==target) return true;
-            else return false;
+        // intitialize base case: if target==0
+        for(int i=0;i<n;i++){
+            dp[i][0]= true;
         }
         
-        if(dp[ind][target]!=-1) return dp[ind][target];
+        // intitialize base case: if ind==0 && target== nums[0]
+        if(target == nums[0]) dp[0][nums[0]]= true;
         
-        //not pick
-        
-        bool notpick= subsetSum(ind-1,target, nums,dp);
-        
-        bool pick= false;
-        if(nums[ind]<=target){
-            pick= subsetSum(ind-1, target-nums[ind], nums,dp);
+        for(int ind=1;ind<n;ind++){
+            for(int k=1;k<=target;k++){
+                //not pick
+                bool notpick= dp[ind-1][k];
+
+                bool pick= false;
+                if(nums[ind]<=k){
+                    pick= dp[ind-1][k-nums[ind]];
+                }
+                dp[ind][k]= pick || notpick;          
+            }
         }
-        
-        return dp[ind][target]= pick || notpick;
+        return dp[n-1][target];       
     }
     bool canPartition(vector<int>& nums) {
         int totSum=0;
@@ -33,8 +37,50 @@ public:
         
         int target= totSum/2;
         
-        vector<vector<int>> dp(n, vector<int>(target+1,-1));
-        
-        return subsetSum(n-1,target, nums,dp);
+        return subsetSum(nums, target,n);
     }
 };
+
+
+// Memoization
+
+// class Solution {
+// public:
+    
+//     bool subsetSum(int ind, int target,vector<int>& nums,vector<vector<int>> &dp){
+//         if(target==0) return true;
+        
+//         if(ind==0){
+//             if(nums[ind]==target) return true;
+//             else return false;
+//         }
+        
+//         if(dp[ind][target]!=-1) return dp[ind][target];
+        
+//         //not pick
+        
+//         bool notpick= subsetSum(ind-1,target, nums,dp);
+        
+//         bool pick= false;
+//         if(nums[ind]<=target){
+//             pick= subsetSum(ind-1, target-nums[ind], nums,dp);
+//         }
+        
+//         return dp[ind][target]= pick || notpick;
+//     }
+//     bool canPartition(vector<int>& nums) {
+//         int totSum=0;
+//         int n= nums.size();
+//         for(int i=0;i<n;i++){
+//             totSum+= nums[i];
+//         }
+        
+//         if(totSum % 2==1) return false; // if TotalSum is odd
+        
+//         int target= totSum/2;
+        
+//         vector<vector<int>> dp(n, vector<int>(target+1,-1));
+        
+//         return subsetSum(n-1,target, nums,dp);
+//     }
+// };
