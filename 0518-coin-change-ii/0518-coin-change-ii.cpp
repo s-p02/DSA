151807dda4422c3c29d80adce53de1e7702be2b3
fudon <80692534+s-p02@users.noreply.github.com>
2022-10-29 22:03:07 +0530

    Time: 100 ms (24.80%), Space: 18.3 MB (53.46%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,23 +1,53 @@
 class Solution {
 public:
-    int count(int ind, int tar,vector<int>& nums,vector<vector<int>>&dp){
-        if(ind==0){
-            if(tar%nums[0]==0) return 1;
-            else return 0;
+    int change(int amount, vector<int>& nums) {
+        
+        int n= nums.size();
+        vector<vector<int>>dp(n, vector<int>(amount+1,0));
+        
+        // base cases:
+        for(int i=0;i<=amount;i++){
+            if(i%nums[0]==0) dp[0][i]=1;
         }
         
-        if(dp[ind][tar]!=-1) return dp[ind][tar];
-        
-        int notpick= count(ind-1, tar,nums,dp);
-        int pick=0;
-        if(nums[ind]<=tar){
-            pick= count(ind,tar-nums[ind],nums,dp);
+        for(int ind=1;ind<n;ind++){
+            for(int tar=0;tar<=amount;tar++){
+                int notpick= dp[ind-1][tar];
+                int pick=0;
+                if(nums[ind]<=tar){
+                    pick= dp[ind][tar-nums[ind]];
+                }
+                dp[ind][tar]= pick+notpick;
+            }
         }
-        return dp[ind][tar]= pick+notpick;
-    }
-    int change(int amount, vector<int>& coins) {
-        int n= coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount+1,-1));
-        return count(n-1,amount,coins,dp);
+        
+        return dp[n-1][amount];
+        
     }
 };
+
+
+// memoization
+// class Solution {
+// public:
+//     int count(int ind, int tar,vector<int>& nums,vector<vector<int>>&dp){
+//         if(ind==0){
+//             if(tar%nums[0]==0) return 1;
+//             else return 0;
+//         }
+        
+//         if(dp[ind][tar]!=-1) return dp[ind][tar];
+        
+//         int notpick= count(ind-1, tar,nums,dp);
+//         int pick=0;
+//         if(nums[ind]<=tar){
+//             pick= count(ind,tar-nums[ind],nums,dp);
+//         }
+//         return dp[ind][tar]= pick+notpick;
+//     }
+//     int change(int amount, vector<int>& coins) {
+//         int n= coins.size();
+//         vector<vector<int>> dp(n, vector<int>(amount+1,-1));
+//         return count(n-1,amount,coins,dp);
+//     }
+// };
