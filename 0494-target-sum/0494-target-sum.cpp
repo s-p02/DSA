f60d38edfa692450ffbc3e7529c5a5233f52dcc2
fudon <80692534+s-p02@users.noreply.github.com>
2022-10-29 21:12:05 +0530

    Time: 25 ms (68.24%), Space: 12.1 MB (49.04%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,23 +1,34 @@
 class Solution {
 public:
-    int count(int ind, int target,vector<int>& nums, vector<vector<int>> &dp){
-        if(ind==0){
-            if(target==0 && nums[0]==0) return 2;
-            if(target==0 || target==nums[0]) return 1;
-            return 0;
+    int count(int ind, int tar,vector<int>& nums, int n){
+        
+        vector<vector<int>>dp(n,vector<int>(tar+1,0));
+        
+        // base Cases:
+        if(nums[0]==0) dp[0][0]=2;
+        else{
+            dp[0][0]=1;
         }
+        if(nums[0]!=0 && nums[0]<= tar) dp[0][nums[0]]=1;
         
-        if(dp[ind][target]!=-1) return dp[ind][target];
+        // table
         
-        int notpick= count(ind-1,target, nums,dp);
-        int pick=0;
-        if(nums[ind]<= target){
-            pick= count(ind-1, target- nums[ind], nums ,dp);
+        for(int ind=1;ind<n;ind++){
+            for(int k=0;k<=tar;k++){
+                
+                int notpick= dp[ind-1][k];
+                int pick=0;
+                if(nums[ind]<= k){
+                    pick= dp[ind-1][k- nums[ind]];
+                }
+                dp[ind][k]= pick+notpick;
+            }
         }
+        return dp[n-1][tar];
         
-        return dp[ind][target]= pick+notpick;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
+        
         // Make the question similar to Count partitions with given difference D
         
         int n= nums.size();
@@ -29,8 +40,45 @@ public:
         if(totSum-target<0 || (totSum-target)%2 != 0) return 0;
         else{
             int modifiedTar= (totSum- target)/2;
-            vector<vector<int>>dp(n,vector<int>(modifiedTar+1,-1));
-            return count(n-1, modifiedTar ,nums,dp);
+            return count(n-1, modifiedTar ,nums,n);
         }
     }
 };
+
+// // Memoization
+// class Solution {
+// public:
+//     int count(int ind, int target,vector<int>& nums, vector<vector<int>> &dp){
+//         if(ind==0){
+//             if(target==0 && nums[0]==0) return 2;
+//             if(target==0 || target==nums[0]) return 1;
+//             return 0;
+//         }
+        
+//         if(dp[ind][target]!=-1) return dp[ind][target];
+        
+//         int notpick= count(ind-1,target, nums,dp);
+//         int pick=0;
+//         if(nums[ind]<= target){
+//             pick= count(ind-1, target- nums[ind], nums ,dp);
+//         }
+        
+//         return dp[ind][target]= pick+notpick;
+//     }
+//     int findTargetSumWays(vector<int>& nums, int target) {
+//         // Make the question similar to Count partitions with given difference D
+        
+//         int n= nums.size();
+//         int totSum=0;
+//         for(int i=0;i<n;i++){
+//             totSum+=nums[i];
+//         }
+        
+//         if(totSum-target<0 || (totSum-target)%2 != 0) return 0;
+//         else{
+//             int modifiedTar= (totSum- target)/2;
+//             vector<vector<int>>dp(n,vector<int>(modifiedTar+1,-1));
+//             return count(n-1, modifiedTar ,nums,dp);
+//         }
+//     }
+// };
