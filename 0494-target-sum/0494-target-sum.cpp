ca38c9d014da24fabf9727e52c4b4f852952ea3f
fudon <80692534+s-p02@users.noreply.github.com>
2022-10-29 21:16:47 +0530

    Time: 21 ms (74.15%), Space: 11.7 MB (55.14%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,30 +1,33 @@
+// Space optimization
 class Solution {
 public:
     int count(int ind, int tar,vector<int>& nums, int n){
         
-        vector<vector<int>>dp(n,vector<int>(tar+1,0));
+        vector<int>prev(tar+1,0);
         
         // base Cases:
-        if(nums[0]==0) dp[0][0]=2;
+        if(nums[0]==0) prev[0]=2;
         else{
-            dp[0][0]=1;
+            prev[0]=1;
         }
-        if(nums[0]!=0 && nums[0]<= tar) dp[0][nums[0]]=1;
+        if(nums[0]!=0 && nums[0]<= tar) prev[nums[0]]=1;
         
         // table
         
         for(int ind=1;ind<n;ind++){
+            vector<int>curr(tar+1,0);
             for(int k=0;k<=tar;k++){
                 
-                int notpick= dp[ind-1][k];
+                int notpick= prev[k];
                 int pick=0;
                 if(nums[ind]<= k){
-                    pick= dp[ind-1][k- nums[ind]];
+                    pick= prev[k- nums[ind]];
                 }
-                dp[ind][k]= pick+notpick;
+                curr[k]= pick+notpick;
             }
+            prev=curr;
         }
-        return dp[n-1][tar];
+        return prev[tar];
         
     }
     int findTargetSumWays(vector<int>& nums, int target) {
@@ -44,6 +47,53 @@ public:
         }
     }
 };
+
+// class Solution {
+// public:
+//     int count(int ind, int tar,vector<int>& nums, int n){
+        
+//         vector<vector<int>>dp(n,vector<int>(tar+1,0));
+        
+//         // base Cases:
+//         if(nums[0]==0) dp[0][0]=2;
+//         else{
+//             dp[0][0]=1;
+//         }
+//         if(nums[0]!=0 && nums[0]<= tar) dp[0][nums[0]]=1;
+        
+//         // table
+        
+//         for(int ind=1;ind<n;ind++){
+//             for(int k=0;k<=tar;k++){
+                
+//                 int notpick= dp[ind-1][k];
+//                 int pick=0;
+//                 if(nums[ind]<= k){
+//                     pick= dp[ind-1][k- nums[ind]];
+//                 }
+//                 dp[ind][k]= pick+notpick;
+//             }
+//         }
+//         return dp[n-1][tar];
+        
+//     }
+//     int findTargetSumWays(vector<int>& nums, int target) {
+        
+//         // Make the question similar to Count partitions with given difference D
+        
+//         int n= nums.size();
+//         int totSum=0;
+//         for(int i=0;i<n;i++){
+//             totSum+=nums[i];
+//         }
+        
+//         if(totSum-target<0 || (totSum-target)%2 != 0) return 0;
+//         else{
+//             int modifiedTar= (totSum- target)/2;
+//             return count(n-1, modifiedTar ,nums,n);
+//         }
+//     }
+// };
 
 // // Memoization
 // class Solution {
