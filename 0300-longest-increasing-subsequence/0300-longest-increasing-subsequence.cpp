188c84849dd69e2ade1b4a1042d2b4de195f72fd
fudon <80692534+s-p02@users.noreply.github.com>
2022-11-11 20:07:35 +0530

    Time: 8 ms (97.15%), Space: 10.5 MB (72.48%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,26 +1,53 @@
-// Tabulation Algorithm
+// Binary Search + DP Solution 
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        int n= nums.size();
-        vector<int>dp(n,1);
-        for(int ind=0;ind<n;ind++){
-            for(int prev=0;prev<ind;prev++){
-                if(nums[prev]<nums[ind]){
-                    dp[ind]=max(dp[ind], 1+dp[prev]);
-                }
+        vector<int> temp;
+        temp.push_back(nums[0]);
+        int len=1;
+        
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]>temp.back()){
+                temp.push_back(nums[i]);
+                len++;
+            }
+            else{
+                int ind= lower_bound(temp.begin(),temp.end(),nums[i])- temp.begin();
+                temp[ind]= nums[i];        
             }
         }
-        
-        // maximum form the dp array:
-        int ans= INT_MIN;
-        for(int i=0;i<n;i++){
-            ans= max(ans,dp[i]);
-        }
-        
-        return ans;
+        return len;
     }
 };
+
+
+
+
+
+
+// // Tabulation Algorithm
+// class Solution {
+// public:
+//     int lengthOfLIS(vector<int>& nums) {
+//         int n= nums.size();
+//         vector<int>dp(n,1);
+//         for(int ind=0;ind<n;ind++){
+//             for(int prev=0;prev<ind;prev++){
+//                 if(nums[prev]<nums[ind]){
+//                     dp[ind]=max(dp[ind], 1+dp[prev]);
+//                 }
+//             }
+//         }
+        
+//         // maximum form the dp array:
+//         int ans= INT_MIN;
+//         for(int i=0;i<n;i++){
+//             ans= max(ans,dp[i]);
+//         }
+        
+//         return ans;
+//     }
+// };
 
 
 
