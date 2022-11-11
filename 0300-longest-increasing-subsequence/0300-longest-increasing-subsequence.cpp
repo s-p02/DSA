16f6bfdb84e41a40abaa0366df2f9938ac770eab
fudon <80692534+s-p02@users.noreply.github.com>
2022-11-11 15:53:50 +0530

    Time: 708 ms (31.82%), Space: 10.7 MB (30.72%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,28 +1,56 @@
-// Tabulation
+// Tabulation Algorithm
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n= nums.size();
-        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
-        
-        // NO need for base case because already entire dp is initialized to 0
-        
-        for(int ind=n-1;ind>=0;ind--){
-            for(int prev_ind=n-1;prev_ind>=-1;prev_ind--){
-                
-                int notpick= 0+ dp[ind+1][prev_ind+1];
-                int pick= INT_MIN;
-                if(prev_ind == -1|| nums[ind]>nums[prev_ind]  ){
-                    pick= 1+ dp[ind+1][ind+1];
+        vector<int>dp(n,1);
+        for(int ind=0;ind<n;ind++){
+            for(int prev=0;prev<ind;prev++){
+                if(nums[prev]<nums[ind]){
+                    dp[ind]=max(dp[ind], 1+dp[prev]);
                 }
-
-                dp[ind][prev_ind+1]=max(pick,notpick);
             }
         }
         
-        return dp[0][-1+1];
+        // maximum form the dp array:
+        int ans= INT_MIN;
+        for(int i=0;i<n;i++){
+            ans= max(ans,dp[i]);
+        }
+        
+        return ans;
     }
 };
+
+
+
+
+
+// // Tabulation
+// class Solution {
+// public:
+//     int lengthOfLIS(vector<int>& nums) {
+//         int n= nums.size();
+//         vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        
+//         // NO need for base case because already entire dp is initialized to 0
+        
+//         for(int ind=n-1;ind>=0;ind--){
+//             for(int prev_ind=n-1;prev_ind>=-1;prev_ind--){
+                
+//                 int notpick= 0+ dp[ind+1][prev_ind+1];
+//                 int pick= INT_MIN;
+//                 if(prev_ind == -1|| nums[ind]>nums[prev_ind]  ){
+//                     pick= 1+ dp[ind+1][ind+1];
+//                 }
+
+//                 dp[ind][prev_ind+1]=max(pick,notpick);
+//             }
+//         }
+        
+//         return dp[0][-1+1];
+//     }
+// };
 
 
 // // Memoization
