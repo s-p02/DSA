d50be4e7be7f40ed59ea394214b7d951ecc86a21
fudon <80692534+s-p02@users.noreply.github.com>
2022-11-30 22:26:41 +0530

    Time: 9 ms (6.37%), Space: 8.4 MB (41.95%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -9,19 +9,47 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+
+// Iterative
 class Solution {
 public:
-    void pre(TreeNode* root, vector<int>& ans){
-        if(root==NULL){
-            return;
-        }
-        ans.push_back(root->val);
-        pre(root->left,ans);
-        pre(root->right,ans);
-    }
     vector<int> preorderTraversal(TreeNode* root) {
         vector<int> ans;
-        pre(root,ans);
+        if(root==NULL){
+            return ans;
+        }
+        stack <TreeNode*>s;
+        s.push(root);   
+        while(!s.empty()){
+            TreeNode* temp= s.top();
+            s.pop();
+            ans.push_back(temp->val);
+            if(temp->right) s.push(temp->right);
+            if(temp->left) s.push(temp->left);
+        }
         return ans;
     }
 };
+
+
+
+// Recursive
+
+
+//class Solution {
+// public:
+//     void pre(TreeNode* root, vector<int>& ans){
+//         if(root==NULL){
+//             return;
+//         }
+//         ans.push_back(root->val);
+//         pre(root->left,ans);
+//         pre(root->right,ans);
+//     }
+//     vector<int> preorderTraversal(TreeNode* root) {
+//         vector<int> ans;
+//         pre(root,ans);
+//         return ans;
+//     }
+// };
