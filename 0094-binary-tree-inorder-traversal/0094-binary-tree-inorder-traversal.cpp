7c7b0bb529b70bf5f7c48c875f950aeccc648bf4
fudon <80692534+s-p02@users.noreply.github.com>
2022-12-07 22:59:35 +0530

    Time: 3 ms (62.74%), Space: 8.4 MB (75.39%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -9,20 +9,61 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+ // iterative all three preorder postorder inorder together
 class Solution {
 public:
-    void inorder(TreeNode* root, vector<int>& ans){
-        if(root== NULL){
-            return;
-        }
-        
-        inorder(root->left,ans);
-        ans.push_back(root->val);
-        inorder(root->right, ans);
-    }
     vector<int> inorderTraversal(TreeNode* root) {
+        
+        stack<pair<TreeNode*,int>>s;
         vector<int> ans;
-        inorder(root,ans);
+        if(root==NULL){
+            return ans;
+        }
+
+        s.push({root,1});
+
+        while(!s.empty()){
+            auto temp= s.top();
+            s.pop();
+            if(temp.second==1){
+                temp.second ++;
+                s.push(temp);
+                if(temp.first->left){
+                    s.push({temp.first->left,1});
+                }
+            }
+            else if(temp.second==2){
+                ans.push_back(temp.first->val);
+                temp.second ++;
+                s.push(temp);
+                if(temp.first->right){
+                    s.push({temp.first->right,1});
+                }
+            }
+            else{
+                continue;
+            }
+        }
         return ans;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     void inorder(TreeNode* root, vector<int>& ans){
+//         if(root== NULL){
+//             return;
+//         }
+        
+//         inorder(root->left,ans);
+//         ans.push_back(root->val);
+//         inorder(root->right, ans);
+//     }
+//     vector<int> inorderTraversal(TreeNode* root) {
+//         vector<int> ans;
+//         inorder(root,ans);
+//         return ans;
+//     }
+// };
