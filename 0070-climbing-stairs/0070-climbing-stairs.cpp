d24c8743e8ae3dff54fd1ea296339433b19997a4
fudon <80692534+s-p02@users.noreply.github.com>
2022-12-12 11:32:24 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (15.04%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> dp(n+1,-1);
+        return recur(n,dp);
+    }
+    int recur(int n,vector<int>&dp){
+        if(n==0) return 1;
+        if(n==1) return 1;
+        
+        if(dp[n]!=-1) return dp[n];
+        return dp[n]= recur(n-1,dp)+recur(n-2,dp);
+    }
+};
