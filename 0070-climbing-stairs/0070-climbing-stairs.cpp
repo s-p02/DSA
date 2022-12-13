6213edba4caced67c19da1d06399c52cb9392ffd
fudon <80692534+s-p02@users.noreply.github.com>
2022-12-13 09:19:04 +0530

    Time: 4 ms (21.11%), Space: 6.3 MB (15.01%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -6,7 +6,7 @@ public:
     }
     int recur(int n,vector<int>&dp){
         if(n==0) return 1;
-        if(n==1) return 1;
+        if(n<0) return 0;
         
         if(dp[n]!=-1) return dp[n];
         return dp[n]= recur(n-1,dp)+recur(n-2,dp);
