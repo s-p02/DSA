33acf46ef64dd9e3f17493ef8bea31bd09c75fcf
Seejjans Parekh <80692534+s-p02@users.noreply.github.com>
2023-02-05 20:06:20 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (45.32%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -9,44 +9,75 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
- // iterative all three preorder postorder inorder together
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> inorder;
         
-        stack<pair<TreeNode*,int>>s;
-        vector<int> ans;
-        if(root==NULL){
-            return ans;
-        }
-
-        s.push({root,1});
-
-        while(!s.empty()){
-            auto temp= s.top();
-            s.pop();
-            if(temp.second==1){
-                temp.second ++;
-                s.push(temp);
-                if(temp.first->left){
-                    s.push({temp.first->left,1});
-                }
-            }
-            else if(temp.second==2){
-                ans.push_back(temp.first->val);
-                temp.second ++;
-                s.push(temp);
-                if(temp.first->right){
-                    s.push({temp.first->right,1});
-                }
+        TreeNode* curr= root;
+        while(curr!=NULL){
+            if(curr->left==NULL){
+                inorder.push_back(curr->val);
+                curr=curr->right;
             }
             else{
-                continue;
+                TreeNode* prev= curr->left;
+                while(prev->right!=NULL && prev->right!=curr){
+                    prev=prev->right;
+                }
+                if(prev->right==NULL){
+                    prev->right=curr;
+                    curr=curr->left;
+                }
+                else{
+                    prev->right=NULL;
+                    inorder.push_back(curr->val);
+                    curr=curr->right;
+                }
             }
         }
-        return ans;
+        return inorder;
     }
 };
+
+
+// class Solution {
+// public:
+//     vector<int> inorderTraversal(TreeNode* root) {
+        
+//         stack<pair<TreeNode*,int>>s;
+//         vector<int> ans;
+//         if(root==NULL){
+//             return ans;
+//         }
+
+//         s.push({root,1});
+
+//         while(!s.empty()){
+//             auto temp= s.top();
+//             s.pop();
+//             if(temp.second==1){
+//                 temp.second ++;
+//                 s.push(temp);
+//                 if(temp.first->left){
+//                     s.push({temp.first->left,1});
+//                 }
+//             }
+//             else if(temp.second==2){
+//                 ans.push_back(temp.first->val);
+//                 temp.second ++;
+//                 s.push(temp);
+//                 if(temp.first->right){
+//                     s.push({temp.first->right,1});
+//                 }
+//             }
+//             else{
+//                 continue;
+//             }
+//         }
+//         return ans;
+//     }
+// };
 
 
 
