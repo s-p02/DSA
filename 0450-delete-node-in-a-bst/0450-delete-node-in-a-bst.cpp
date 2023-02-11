92fac54d4ea676019c91b722f58a8787062c4a3d
Seejjans Parekh <80692534+s-p02@users.noreply.github.com>
2023-02-11 19:36:04 +0530

    Time: 43 ms (42.66%), Space: 32.7 MB (84.00%) - LeetHub

---
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -0,0 +1,67 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        
+        
+        if(root==NULL){
+            return NULL;
+        }
+        
+        if(root->val==key){
+            return helper(root);
+        }
+        
+        TreeNode* dummy= root;
+        while(root!=NULL){
+            if(key>root->val){
+                if(root->right !=NULL && root->right->val==key){
+                    root->right=helper(root->right);
+                    break;
+                }
+                else{
+                    root=root->right;
+                }
+            }
+            else{
+                if(root->left !=NULL && root->left->val==key){
+                    root->left=helper(root->left);
+                    break;
+                }
+                else{
+                    root=root->left;
+                }
+            }
+        }
+        return dummy;
+    }
+    
+    TreeNode* helper(TreeNode* root){
+        if(root->left==NULL) return root->right;
+        if(root->right== NULL) return root->left;
+        
+        TreeNode* rightchild= root->right;
+        TreeNode* leftMostChild= find(root->left);
+        leftMostChild->right= rightchild;
+        
+        return root->left;
+    }
+    
+    TreeNode* find(TreeNode* root){
+        if(root->right==NULL){
+            return root;
+        }
+        return find(root->right);
+    }
+        
+};
