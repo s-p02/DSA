5776c104fa900792d932beefd00965483d570d80
Seejjans Parekh <80692534+s-p02@users.noreply.github.com>
2023-04-15 10:39:29 +0530

    Time: 31 ms (97.59%), Space: 13.3 MB (16.99%) - LeetHub

--- 2428-maximum-sum-of-an-hourglass/2428-maximum-sum-of-an-hourglass.cpp
+++ 2428-maximum-sum-of-an-hourglass/2428-maximum-sum-of-an-hourglass.cpp
@@ -16,12 +16,7 @@ public:
         int maxi=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(check(i-1,j-1,grid)&&
-                check(i-1,j,grid) &&
-                check(i-1,j+1,grid)&&
-                check(i+1,j-1,grid)&&
-                check(i+1,j,grid)&&
-                check(i+1,j+1,grid)){
+                if(i-1>=0 && i+1<n && j-1>=0 && j+1<m){
                     int sum= grid[i-1][j-1]+grid[i-1][j]+grid[i-1][j+1]+grid[i+1][j-1]+grid[i+1][j]+grid[i+1][j+1]+grid[i][j];
                     maxi=max(maxi,sum);
                 }
