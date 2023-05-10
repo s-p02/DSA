8fc18df694e9a0084d8ed99fcf2b59c8e7db782b
Seejjans Parekh <80692534+s-p02@users.noreply.github.com>
2023-05-10 19:23:10 +0530

    Time: 19 ms (7.69%), Space: 14.2 MB (37.35%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -18,7 +18,29 @@ public:
         int oldc=image[sr][sc];
         int delRow[] = {-1, 0, +1, 0};
         int delCol[] = {0, +1, 0, -1}; 
-        dfs(sr,sc,image,newc,delRow,delCol,oldc);
+        int n= image.size();
+        int m= image[0].size();
+        vector<vector<int>> vis(n,vector<int>(m,0));
+        dfs1(sr,sc,image,newc,delRow,delCol,oldc,vis);
         return image;
     }
+    
+    void dfs1(int row, int col,vector<vector<int>>& image, int newColor, int delRow[], int delCol[],
+     int iniColor, vector<vector<int>>& vis){
+        vis[row][col]=1;
+        image[row][col]=newColor;
+        
+        for(int i=0;i<4;i++){
+            int nrow= row+ delRow[i];
+            int ncol= col+ delCol[i];
+            
+            if(nrow>=0 && nrow<image.size() && ncol>=0 && ncol<image[0].size() && vis[nrow][ncol]!=1                    && image[nrow][ncol]== iniColor){
+                dfs1(nrow, ncol, image, newColor, delRow, delCol, iniColor, vis);
+            }
+        }
+    }
+        
+        
+        
+        
 };
