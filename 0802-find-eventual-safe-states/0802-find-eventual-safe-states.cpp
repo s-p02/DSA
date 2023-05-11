3b88f3f863ab35d5bef499775666058c628c001c
Seejjans Parekh <80692534+s-p02@users.noreply.github.com>
2023-05-11 18:36:14 +0530

    Time: 165 ms (66.87%), Space: 46.6 MB (97.94%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,32 +1,8 @@
 class Solution {
 public:
-    
-    bool dfs(int node, vector<vector<int>>& graph, int vis[], int pathvis[], int check[]){
-        vis[node]=1;
-        pathvis[node]=1;
-        check[node]=0;
-        
-        // adjacent
-        for(auto it: graph[node]){
-            if(!vis[it]){
-                if(dfs(it,graph,vis,pathvis,check)==true){
-                    return true;
-                }
-            }
-            else if(pathvis[it]==1){
-                return true;
-            }
-        }
-        
-        check[node]=1;
-        pathvis[node]=0;
-        return false;
-    }
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n= graph.size();
-        int vis[n];
-        int pathvis[n];
-        int check[n];
+        int vis[n],pathvis[n], check[n];
         for(int i=0;i<n;i++){
             vis[i]=0;
             pathvis[i]=0;
@@ -35,16 +11,40 @@ public:
         
         for(int i=0;i<n;i++){
             if(!vis[i]){
-                dfs(i,graph,vis,pathvis,check);
+                dfs(i, graph, vis, pathvis, check);
             }
         }
+        
         vector<int> res;
         for(int i=0;i<n;i++){
             if(check[i]==1){
                 res.push_back(i);
             }
         }
-        
         return res;
+    }
+    
+    bool dfs(int node,vector<vector<int>>& graph,int vis[],int pathvis[],int check[] ){
+        vis[node]=1;
+        pathvis[node]=1;
+        check[node]=0;
+        
+        for(auto it: graph[node]){
+            if(!vis[it]){
+                if(dfs(it, graph, vis, pathvis, check)==true){
+                   return true; 
+                }
+            }
+            else if(pathvis[it]==1){
+                return true;
+            }
+            
+        }
+        
+        // After all dfs calls have been completed and there is no cycle then we can mark the node as safe node
+        
+        check[node]=1;
+        pathvis[node]=0;
+        return false;
     }
 };
