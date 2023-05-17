1e1dbdea45491df1277bdd8b43fa96e768485662
Seejjans Parekh <80692534+s-p02@users.noreply.github.com>
2023-05-17 23:26:58 +0530

    Time: 3 ms (97.97%), Space: 8.8 MB (54.17%) - LeetHub

---
+++ 2682-find-the-losers-of-the-circular-game/2682-find-the-losers-of-the-circular-game.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> circularGameLosers(int n, int k) {
+        
+        vector<int> vis(n+1,0);
+        int ball=1;
+        bool flag=true;
+        vis[ball]=1;
+        int step=1;
+        while(flag){
+            if((ball+(step*k))%n==0){
+                ball=n;
+            }
+            else{
+                ball= (ball + (step*k))%n;
+            }
+            vis[ball]++;
+            if(vis[ball]==2){
+                flag=false;
+                break;
+            }
+            step++;
+        }
+        vector<int> res;
+        for(int i=1;i<vis.size();i++){
+            if(vis[i]==0){
+                res.push_back(i);
+            }
+        }
+        sort(res.begin(),res.end());
+        return res;
+    }
+};
