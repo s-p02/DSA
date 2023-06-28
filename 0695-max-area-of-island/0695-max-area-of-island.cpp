3e131c4dce12f58af1586163c426222bd77bba47
Seejjans Parekh <80692534+s-p02@users.noreply.github.com>
2023-06-28 23:15:55 +0530

    Time: 28 ms (32.27%), Space: 23.9 MB (39.57%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -1,70 +1,72 @@
-// class DisjointSet{
-//     public:
-//     vector<int> size, parent;
-//     DisjointSet(int n){
-//         size.resize(n+1,0);
-//         parent.resize(n+1);
-//         for(int i=0;i<n;i++){
-//             parent[i]=i;
-//         }
-//     }
-    
-//     int findUPar(int node){
-//         if(parent[node]==node){
-//             return node;
-//         }
-//         return parent[node]= findUPar(parent[node]);
-//     }
-    
-//     void unionbysize(int u, int v){
-//         int ulpu= findUPar(u);
-//         int ulpv= findUPar(v);
-//         if(ulpu==ulpv) return;
-//         if(size[ulpu]<size[ulpv]){
-//             parent[ulpu]=ulpv;
-//             size[ulpv]+=size[ulpu];
-//         }
-//         else{
-//             parent[ulpv]=ulpu;
-//             size[ulpu]+=size[ulpv];
-//         }
-//     }
-    
-// };
-class DisjointSet {
+// This can be done using DFS(RECURSION) BUT DSU PROVIDES BETTER TC
 
-public:
-    vector<int> rank, parent, size;
-    DisjointSet(int n) {
-        rank.resize(n + 1, 0);
-        parent.resize(n + 1);
-        size.resize(n + 1);
-        for (int i = 0; i <= n; i++) {
-            parent[i] = i;
-            size[i] = 1;
+class DisjointSet{
+    public:
+    vector<int> size, parent;
+    DisjointSet(int n){
+        size.resize(n+1,1);
+        parent.resize(n+1);
+        for(int i=0;i<=n;i++){
+            parent[i]=i;
         }
     }
-
-    int findUPar(int node) {
-        if (node == parent[node])
+    
+    int findUPar(int node){
+        if(parent[node]==node){
             return node;
-        return parent[node] = findUPar(parent[node]);
-    }
-
-    void unionbysize(int u, int v) {
-        int ulp_u = findUPar(u);
-        int ulp_v = findUPar(v);
-        if (ulp_u == ulp_v) return;
-        if (size[ulp_u] < size[ulp_v]) {
-            parent[ulp_u] = ulp_v;
-            size[ulp_v] += size[ulp_u];
         }
-        else {
-            parent[ulp_v] = ulp_u;
-            size[ulp_u] += size[ulp_v];
+        return parent[node]= findUPar(parent[node]);
+    }
+    
+    void unionbysize(int u, int v){
+        int ulpu= findUPar(u);
+        int ulpv= findUPar(v);
+        if(ulpu==ulpv) return;
+        if(size[ulpu]<size[ulpv]){
+            parent[ulpu]=ulpv;
+            size[ulpv]+=size[ulpu];
+        }
+        else{
+            parent[ulpv]=ulpu;
+            size[ulpu]+=size[ulpv];
         }
     }
+    
 };
+// class DisjointSet {
+
+// public:
+//     vector<int> parent, size;
+//     DisjointSet(int n) {
+//         rank.resize(n + 1, 0);
+//         parent.resize(n + 1);
+//         size.resize(n + 1);
+//         for (int i = 0; i <= n; i++) {
+//             parent[i] = i;
+//             size[i] = 1;
+//         }
+//     }
+
+//     int findUPar(int node) {
+//         if (node == parent[node])
+//             return node;
+//         return parent[node] = findUPar(parent[node]);
+//     }
+
+//     void unionBySize(int u, int v) {
+//         int ulp_u = findUPar(u);
+//         int ulp_v = findUPar(v);
+//         if (ulp_u == ulp_v) return;
+//         if (size[ulp_u] < size[ulp_v]) {
+//             parent[ulp_u] = ulp_v;
+//             size[ulp_v] += size[ulp_u];
+//         }
+//         else {
+//             parent[ulp_v] = ulp_u;
+//             size[ulp_u] += size[ulp_v];
+//         }
+//     }
+// };
 class Solution {
 public:
     
