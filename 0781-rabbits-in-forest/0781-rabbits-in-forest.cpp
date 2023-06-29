543f145697e8237259b854786c20c3844ced02bb
Seejjans Parekh <80692534+s-p02@users.noreply.github.com>
2023-06-29 19:18:25 +0530

    Time: 9 ms (25.38%), Space: 8.6 MB (67.68%) - LeetHub

--- 0781-rabbits-in-forest/0781-rabbits-in-forest.cpp
+++ 0781-rabbits-in-forest/0781-rabbits-in-forest.cpp
@@ -1,25 +1,50 @@
+// Single Pass solution with same logic
 class Solution {
 public:
     int numRabbits(vector<int>& answers) {
         if(answers.empty()) return 0;
         unordered_map<int,int> mpp;
         int cnt=0;
-        
         for(int it:answers){
             if(it==0) cnt++;
             else{
+                if(mpp[it]==0) cnt+=(it+1);
                 mpp[it]++;
                 if(mpp[it]==it+1){
-                    cnt+=(it+1);
                     mpp[it]=0;
                 }
-            }
-        }
-        for(auto it: mpp){
-            if(it.second!=0){
-                cnt+= (it.first+1);
             }
         }
         return cnt;
     }
 };
+
+
+
+// Double Pass But same can be converted to single pass solution with same logic just slight modification
+ 
+// class Solution {
+// public:
+//     int numRabbits(vector<int>& answers) {
+//         if(answers.empty()) return 0;
+//         unordered_map<int,int> mpp;
+//         int cnt=0;
+        
+//         for(int it:answers){
+//             if(it==0) cnt++;
+//             else{
+//                 mpp[it]++;
+//                 if(mpp[it]==it+1){
+//                     cnt+=(it+1);
+//                     mpp[it]=0;
+//                 }
+//             }
+//         }
+//         for(auto it: mpp){
+//             if(it.second!=0){
+//                 cnt+= (it.first+1);
+//             }
+//         }
+//         return cnt;
+//     }
+// };
