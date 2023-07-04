8be11f21a5b35596dc0b6138cb908b12e9947bf8
Seejjans Parekh <80692534+s-p02@users.noreply.github.com>
2023-07-04 18:55:32 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (76.40%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,91 +1,25 @@
-//Memoization:
 class Solution {
 public:
-    int recur(int ind,vector<int>& nums,vector<int>&dp){
-        if(ind==0) return nums[ind];
-        if(ind<0) return 0;
-        
-        if(dp[ind]!=-1) return dp[ind];
-        
+    int rob(vector<int>& nums) {
+        vector<int> dp(nums.size()+1,-1);
+        int ans= recur(nums.size()-1,nums,dp);
+        return ans;
+    }
+    int recur(int ind, vector<int>& nums, vector<int>&dp){
+        if(ind<0){
+            return 0;
+        }
+        if(ind==0){
+            return nums[ind];
+        }
+        if(dp[ind]!=-1){
+            return dp[ind];
+        }
         //pick
         int pick= nums[ind]+ recur(ind-2,nums,dp);
         
         //notpick
-        int notpick= 0+ recur(ind-1,nums,dp);
-        
-        return dp[ind]= max(pick,notpick);
-        
-    }
-    int rob(vector<int>& nums) {
-        int n= nums.size();
-        vector<int>dp(n+1,-1);
-        return recur(n-1,nums,dp);
+        int notpick= recur(ind-1,nums,dp);
+        return dp[ind]=max(pick, notpick);
     }
 };
-
-
-// // Space Optimization
-// class Solution {
-// public:
-//     int rob(vector<int>& nums) {
-//         int n= nums.size();
-//         int prev= nums[0];
-//         int prev2=0;
-//         for(int i=1;i<n;i++){
-//             int pick= nums[i];
-//             if(i>1){
-//                 pick+=prev2;
-//             }
-//             int notpick= 0+prev;
-//             int curr=max(pick,notpick);
-//             prev2=prev;
-//             prev=curr;
-//         }
-//         return prev;
-//     }
-// };
-
-
-// // Tablulation
-// class Solution {
-// public:
-//     int rob(vector<int>& nums) {
-//         int n= nums.size();
-//         vector<int> dp(n,-1);
-//         dp[0]=nums[0];
-//         for(int i=1;i<n;i++){
-//             int pick= nums[i];
-//             if(i>1){
-//                 pick+=dp[i-2];
-//             }
-//             int notpick= 0+dp[i-1];
-//             dp[i]=max(pick,notpick);
-//         }
-//         return dp[n-1];
-//     }
-// };
-
-// //Memoization:
-// class Solution {
-// public:
-//     int recur(int ind,vector<int>& nums,vector<int>&dp){
-//         if(ind==0) return nums[ind];
-//         if(ind<0) return 0;
-        
-//         if(dp[ind]!=-1) return dp[ind];
-        
-//         //pick
-//         int pick= nums[ind]+ recur(ind-2,nums,dp);
-        
-//         //notpick
-//         int notpick= 0+ recur(ind-1,nums,dp);
-        
-//         return dp[ind]= max(pick,notpick);
-        
-//     }
-//     int rob(vector<int>& nums) {
-//         int n= nums.size();
-//         vector<int>dp(n+1,-1);
-//         return recur(n-1,nums,dp);
-//     }
-// };
