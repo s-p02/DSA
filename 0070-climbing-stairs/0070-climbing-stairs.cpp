e9f0f9d36c5817108130d43b37537c203c0a5b22
Seejjans Parekh <80692534+s-p02@users.noreply.github.com>
2023-07-04 17:59:49 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (36.78%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,19 +1,38 @@
+// Tabulation
 class Solution {
 public:
     int climbStairs(int n) {
-        vector<int> dp(n+1,-1);
-        int ans= recur(n,dp);
-        return ans;
-    }
-    int recur(int n,vector<int> &dp){
-        if(n==1) return 1;
-        if(n==2) return 2;
-            
-        if(dp[n]!=-1){
-            return dp[n];
+        vector<int> dp(n+2,-1);
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3;i<=n;i++){
+            int onestep= dp[i-1];
+            int twostep= dp[i-2];
+            dp[i]= onestep+twostep;
         }
-        int onestep= recur(n-1,dp);
-        int twostep= recur(n-2,dp);
-        return dp[n]= onestep+twostep;
+        return dp[n];
     }
 };
+
+
+// Memoization
+
+// class Solution {
+// public:
+//     int climbStairs(int n) {
+//         vector<int> dp(n+1,-1);
+//         int ans= recur(n,dp);
+//         return ans;
+//     }
+//     int recur(int n,vector<int> &dp){
+//         if(n==1) return 1;
+//         if(n==2) return 2;
+            
+//         if(dp[n]!=-1){
+//             return dp[n];
+//         }
+//         int onestep= recur(n-1,dp);
+//         int twostep= recur(n-2,dp);
+//         return dp[n]= onestep+twostep;
+//     }
+// };
