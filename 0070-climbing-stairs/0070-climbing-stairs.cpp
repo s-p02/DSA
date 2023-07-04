80d38b012481ddd11a679ba7452342f3c45a7c6c
Seejjans Parekh <80692534+s-p02@users.noreply.github.com>
2023-07-04 15:11:34 +0530

    Time: 4 ms (13.12%), Space: 6.2 MB (14.08%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -2,13 +2,18 @@ class Solution {
 public:
     int climbStairs(int n) {
         vector<int> dp(n+1,-1);
-        return recur(n,dp);
+        int ans= recur(n,dp);
+        return ans;
     }
-    int recur(int n,vector<int>&dp){
-        if(n==0) return 1;
-        if(n<0) return 0;
-        
-        if(dp[n]!=-1) return dp[n];
-        return dp[n]= recur(n-1,dp)+recur(n-2,dp);
+    int recur(int n,vector<int> &dp){
+        if(n==0 || n==1){
+            return 1;
+        }
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        int onestep= recur(n-1,dp);
+        int twostep= recur(n-2,dp);
+        return dp[n]= onestep+twostep;
     }
 };
