54e17a57436cd58cb60a807523c92ab575265e4c
Seejjans Parekh <80692534+s-p02@users.noreply.github.com>
2023-07-13 16:57:46 +0530

    Time: 13 ms (10.37%), Space: 9 MB (47.82%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,25 +1,64 @@
-// space optimization
 class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n= triangle.size();
-        vector<int> nex(n,0);
-        //base case:
-        for(int j=0;j<n;j++){
-            nex[j]= triangle[n-1][j];
+        vector<vector<int>>dp(n,vector<int>(n,0));
+        for(int i=0;i<n;i++){
+            dp[n-1][i]= triangle[n-1][i];
         }
         for(int i=n-2;i>=0;i--){
-            vector<int> curr(n,0);
             for(int j=0;j<=i;j++){
-                int down= triangle[i][j]+nex[j];
-                int diag= triangle[i][j]+nex[j+1];
-                curr[j]=min(down,diag);
+                int down= triangle[i][j]+dp[i+1][j];
+                int dr=triangle[i][j]+dp[i+1][j+1];
+                dp[i][j]=min(down,dr);
             }
-            nex=curr;
         }
-        return nex[0];
+        return dp[0][0];
     }
 };
+
+
+// class Solution {
+// public:
+//     int recur(int i,int j,vector<vector<int>>& triangle,int n,vector<vector<int>>& dp){
+//         if(i==n-1){
+//             return triangle[i][j];
+//         }
+//         if(dp[i][j]!=-1) return dp[i][j];
+//         int down= triangle[i][j]+recur(i+1,j,triangle,n,dp);
+//         int dr=triangle[i][j]+recur(i+1,j+1,triangle,n,dp);
+//         return dp[i][j]=min(down,dr);
+//     }
+//     int minimumTotal(vector<vector<int>>& triangle) {
+//         int n= triangle.size();
+//         vector<vector<int>> dp(n,vector<int>(n,-1));
+//         return recur(0,0,triangle,n,dp);
+//     }
+// };
+
+
+// // space optimization
+// class Solution {
+// public:
+//     int minimumTotal(vector<vector<int>>& triangle) {
+//         int n= triangle.size();
+//         vector<int> nex(n,0);
+//         //base case:
+//         for(int j=0;j<n;j++){
+//             nex[j]= triangle[n-1][j];
+//         }
+//         for(int i=n-2;i>=0;i--){
+//             vector<int> curr(n,0);
+//             for(int j=0;j<=i;j++){
+//                 int down= triangle[i][j]+nex[j];
+//                 int diag= triangle[i][j]+nex[j+1];
+//                 curr[j]=min(down,diag);
+//             }
+//             nex=curr;
+//         }
+//         return nex[0];
+//     }
+// };
 
 // // Tabulation
 // class Solution {
